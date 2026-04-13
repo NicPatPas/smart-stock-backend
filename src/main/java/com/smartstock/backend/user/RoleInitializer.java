097aa96a05ee5513d9b421e68c78091ac9925d1c
Bergdoktor258 <nicobauer.p@icloud.com>
2026-04-13 19:10:35 +0200

    TESTDATA

--- src/main/java/com/smartstock/backend/user/RoleInitializer.java
+++ src/main/java/com/smartstock/backend/user/RoleInitializer.java
@@ -2,9 +2,11 @@ package com.smartstock.backend.user;
 
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
+@Order(1)
 public class RoleInitializer implements ApplicationRunner {
 
     private final RoleRepository roleRepository;
